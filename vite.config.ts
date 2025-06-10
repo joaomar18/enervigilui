918ef3cc6558d44d441d034eb6ce7b37b5e338bc
joaomar18 <joao.marques18jm@gmail.com>
2025-06-10 17:00:07 +0100

    up

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
         port: 8080,
         proxy: {
             "/api": {
-                target: "http://localhost:8000", // your FastAPI backend
+                target: "http://192.168.0.20:8000", // your FastAPI backend
                 changeOrigin: true,
                 secure: false, // if not using HTTPS
                 rewrite: (path) => path.replace(/^\/api/, ""), // <- this line is critical!
