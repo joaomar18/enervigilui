478df2483f7fbd426c8182f268cdfad3efa51091
joaomar18 <joao.marques18jm@gmail.com>
2025-06-10 16:31:07 +0100

    settings to host from rpi

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,11 @@ import { defineConfig } from "vite";
 export default defineConfig({
     plugins: [sveltekit()],
     server: {
+        host: true,
+        port: 8080,
         proxy: {
             "/api": {
-                target: "http://192.168.0.20:8000", // your FastAPI backend
+                target: "http://localhost:8000", // your FastAPI backend
                 changeOrigin: true,
                 secure: false, // if not using HTTPS
 				rewrite: path => path.replace(/^\/api/, '') // <- this line is critical!
