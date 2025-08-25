e4d826d5eddfebf01313320db9861a31a6e57864
joaomar18 <joao.marques18jm@gmail.com>
2025-08-25 20:04:00 +0100

    up

--- src/lib/types/device/protocol.ts
+++ src/lib/types/device/protocol.ts
@@ -1,12 +1,26 @@
-import type { ProtocolOptionsBase } from "./base";
+import type { Protocol, ProtocolOptionsBase } from "./base";
+import type { SvelteComponent } from "svelte";
 
 export interface ProtocolPlugin {
     protocolText: string,
     protocolInfoText: string,
     defaultOptions: ProtocolOptionsBase,
+    OptionsComponent: typeof SvelteComponent,
 }
 
 
 export class ProtocolRegistry {
+    protocols: Map<string, ProtocolPlugin>;
 
+    constructor() {
+        this.protocols = new Map<string, ProtocolPlugin>();
+    }
+
+    registerPlugin(protocol: Protocol, plugin: ProtocolPlugin) {
+        this.protocols.set(protocol, plugin);
+    }
+
+    getPlugin(protocol: Protocol): ProtocolPlugin | undefined {
+        return this.protocols.get(protocol);
+    }
 }
