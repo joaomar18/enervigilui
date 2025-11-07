912b185c0ef18df65b3ed04b43fbb635114e2007
joaomar18 <joao.marques18jm@gmail.com>
2025-11-07 10:03:42 +0000

    up

--- src/lib/style/general.ts
+++ src/lib/style/general.ts
@@ -464,7 +464,7 @@ const ContentCardStyleConfig: ComponentStyles = {
         borderColor: "rgba(255,255,255,0.07)",
         shadow: "0 8px 16px rgba(0, 0, 0, 0.1)",
         borderRadius: "20px",
-        titleSize: "1.0625rem",
+        titleSize: "17px",
         titleColor: "#f5f5f5",
         titleWeight: "500",
         titlePaddingLeft: "10px",
