e4adee098524743b138a96cc517579d40afe55d7
joaomar18 <joao.marques18jm@gmail.com>
2026-02-02 21:43:49 +0000

    need to create centralized sse logic orchestrator.

--- src/routes/health/system/+page.svelte
+++ src/routes/health/system/+page.svelte
@@ -2,25 +2,50 @@
     import { onMount, onDestroy } from "svelte";
 
     // Variables
-    let eventSource: EventSource | null = null;
+    let realtime_eventSource: EventSource | null = null;
+    let cpu_usage_eventSource: EventSource | null = null;
+    let ram_usage_eventSource: EventSource | null = null;
 
     // Navigation
     import { resetDashboardLoader } from "$lib/logic/view/navigation";
 
     onMount(() => {
         resetDashboardLoader();
-        eventSource = new EventSource("/sse/system/get_realtime_metrics");
-        eventSource.onmessage = function (event) {
+        realtime_eventSource = new EventSource("/sse/system/get_realtime_metrics");
+        cpu_usage_eventSource = new EventSource("/sse/system/get_cpu_usage_history");
+        ram_usage_eventSource = new EventSource("/sse/system/get_ram_usage_history");
+
+        realtime_eventSource.onmessage = function (event) {
             console.log(event.data);
         };
-        eventSource.onerror = function () {
+        cpu_usage_eventSource.onmessage = function (event) {
+            console.log(event.data);
+        };
+        ram_usage_eventSource.onmessage = function (event) {
+            console.log(event.data);
+        };
+        realtime_eventSource.onerror = function () {
+            console.error("SSE connection error");
+        };
+        cpu_usage_eventSource.onerror = function () {
+            console.error("SSE connection error");
+        };
+        ram_usage_eventSource.onerror = function () {
             console.error("SSE connection error");
         };
     });
     onDestroy(() => {
-        if (eventSource) {
-            eventSource.close();
-            eventSource = null;
+        if (realtime_eventSource) {
+            realtime_eventSource.close();
+            realtime_eventSource = null;
+        }
+        if (cpu_usage_eventSource) {
+            cpu_usage_eventSource.close();
+            cpu_usage_eventSource = null;
+        }
+        if (ram_usage_eventSource) {
+            ram_usage_eventSource.close();
+            ram_usage_eventSource = null;
         }
     });
 </script>
