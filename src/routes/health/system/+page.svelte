d2c5f5f88a8bc5a897f61c32fad299feab175a90
joaomar18 <joao.marques18jm@gmail.com>
2026-02-08 11:47:00 +0000

    added disk_read and disk_write speed sse endpoints.

--- src/routes/health/system/+page.svelte
+++ src/routes/health/system/+page.svelte
@@ -5,28 +5,39 @@
 
     // Navigation
     import { resetDashboardLoader } from "$lib/logic/view/navigation";
+    import { loadedDone } from "$lib/stores/view/navigation";
 
     // Variables
     let realtimeStream: SSEHandler<RealTimeSystemData> | null = null;
     let cpuHistoryStream: SSEHandler<number[]> | null = null;
     let ramHistoryStream: SSEHandler<number[]> | null = null;
+    let diskReadSpeedHistoryStream: SSEHandler<number[]> | null = null;
+    let diskWriteSpeedHistoryStream: SSEHandler<number[]> | null = null;
     let realtimeData: RealTimeSystemData | undefined = undefined;
     let cpuHistoryData: number[] | undefined = undefined;
     let ramHistoryData: number[] | undefined = undefined;
+    let diskReadSpeedData: number[] | undefined = undefined;
+    let diskWriteSpeedData: number[] | undefined = undefined;
 
     // Reactive statements
     $: console.log(realtimeData);
     $: console.log(cpuHistoryData);
     $: console.log(ramHistoryData);
+    $: console.log(diskReadSpeedData);
+    $: console.log(diskWriteSpeedData);
 
     // Functions
     function closeAllStreams() {
         realtimeStream?.close();
         cpuHistoryStream?.close();
         ramHistoryStream?.close();
+        diskReadSpeedHistoryStream?.close();
+        diskWriteSpeedHistoryStream?.close();
         realtimeStream = null;
         cpuHistoryStream = null;
         ramHistoryStream = null;
+        diskReadSpeedHistoryStream = null;
+        diskWriteSpeedHistoryStream = null;
     }
 
     onMount(() => {
@@ -54,6 +65,21 @@
             },
             3000,
         );
+        diskReadSpeedHistoryStream = new SSEHandler(
+            "/sse/system/get_disk_read_speed_history",
+            (data: number[]) => {
+                diskReadSpeedData = data;
+            },
+            3000,
+        );
+        diskWriteSpeedHistoryStream = new SSEHandler(
+            "/sse/system/get_disk_write_speed_history",
+            (data: number[]) => {
+                diskWriteSpeedData = data;
+            },
+            3000,
+        );
+        loadedDone.set(true);
     });
     onDestroy(() => {
         window.removeEventListener("beforeunload", closeAllStreams);
